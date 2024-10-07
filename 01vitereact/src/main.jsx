d498acec12d20b49d2372d9f7fc1698885cf61ad
Sarang <gaikwadsarang111@gmail.com>
2024-10-07 10:53:20 +0530

    custome react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  render (
+    <div><h1>Custom App</h1></div>
+  )
+}
+const ReactElement = {
+  type: "a",
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+createRoot(document.getElementById('root')).
+render(
+  ReactElement
 )
