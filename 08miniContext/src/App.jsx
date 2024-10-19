9e8a575f0c08a91fbd20ecb3b22b7fac1a6fb96d
Sarang <gaikwadsarang111@gmail.com>
2024-10-19 18:54:10 +0530

    Context API

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+ 
+
+  return (
+   <UserContextProvider>
+   <h1>React</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
